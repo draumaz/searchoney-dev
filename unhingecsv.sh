e9273a33baa5e5375d8ab7504cac51a53c491686
Emma <77967471+draumaz@users.noreply.github.com>
2023-07-07 17:58:03 -0700

    he

--- unhingecsv.sh
+++ unhingecsv.sh
@@ -5,5 +5,5 @@ for i in *.csv; do
   for j in `cat "${i}" | awk -F';' '{print $1}'`; do
     k="$(echo ${j} | sed s/,/:/)"
     sed -i "s/${j}j/${k}/g" "${i}"
-  done; printf $i
+  done; printf "${i}"
 done
