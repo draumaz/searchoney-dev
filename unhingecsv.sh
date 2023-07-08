6e3ee81e92ad87a0fbc3547c32c008a93b01f75a
Emma <77967471+draumaz@users.noreply.github.com>
2023-07-08 10:52:40 -0700

    doesnt work

--- unhingecsv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd TRANSCRIPTS/${1}
-for i in *.csv; do
-  for j in `cat "${i}" | awk -F';' '{print $1}'`; do
-    k="$(echo ${j} | sed s/,/:/)"
-    sed -i "s/${j}j/${k}/g" "${i}"
-  done; printf "${i}"
-done
