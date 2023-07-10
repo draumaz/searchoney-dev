2384aa6f14fde34cd4de09f3aa6e9f9d4a686774
Emma <77967471+draumaz@users.noreply.github.com>
2023-07-10 14:25:36 -0700

    Update csv-jsonner.sh

--- csv-jsonner.sh
+++ csv-jsonner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for i in TRANSCRIPTS/*/*.csv; do
-  csvjson -d ';' "$i" > "$i.json.pre"
+  csvjson -d ';' "${i}" > "${i}.json.pre"
   cat >> "${i}".json << EOF
 { "Data":
 `cat "${i}.json.pre" | jq`
 }
 EOF
-  rm -v "$i.json.pre"
+  rm -v "${i}.json.pre"
 done
