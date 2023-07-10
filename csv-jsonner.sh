e81614ea47cff871f5c86caea0096235e82495f9
Emma <77967471+draumaz@users.noreply.github.com>
2023-07-10 15:21:56 -0700

    prep

--- csv-jsonner.sh
+++ csv-jsonner.sh
@@ -2,7 +2,7 @@
 
 for i in TRANSCRIPTS/*/*.csv; do
   csvjson -d ';' "${i}" > "${i}.json.pre"
-  cat >> "${i}".json << EOF
+  cat >> "json/${i}".json << EOF
 { "Data":
 `cat "${i}.json.pre" | jq`
 }
